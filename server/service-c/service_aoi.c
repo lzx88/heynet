6ee69efc718ad19132eb6f81ed26c30d01f48529
joelv(吕志雄) <joelv@KUGOU.GZ>
2017-02-27 14:45:06 +0800

    fdf

--- server/service-c/service_aoi.c
+++ server/service-c/service_aoi.c
@@ -8,11 +8,10 @@ typedef uint8 GIDX;
 typedef struct Entity{
 	struct Entity* next;
 	uint32 id;
+	uint16 r;//视野半径
+	GIDX gidx;
 	uint16 x;
 	uint16 y;
-	GIDX gx;
-	GIDX gy;
-	uint16 r;//视野半径
 }*EntityPtr;
 
 typedef struct Grid{
@@ -23,22 +22,82 @@ typedef struct Grid{
 typedef struct AOI{
 	uint16 mx;
 	uint16 my;
-	uint16 gx_bit;
-	uint16 gy_bit;
-	GIDX xlen;
-	GIDX ylen;
+	uint16 gxbit;
+	uint16 gybit;
+	GIDX gxlen;
+	GIDX gylen;
 	GridPtr vGrid;
 }*AOIPtr;
 
 void (aoi_cb*)(uint32 watcher, uint32 entity);
-void cb_appear(uint32 watcher, uint32 entity){
+void
+cb_appear(uint32 watcher, uint32 entity){
 	printf("%d watch %d appear!", watcher, entity);
 }
-void cb_disappear(uint32 watcher, uint32 entity){
+void
+cb_disappear(uint32 watcher, uint32 entity){
 	printf("%d watch %d disappear!", watcher, entity);
 }
-void cb_move(uint32 watcher, uint32 entity){
+void
+cb_move(uint32 watcher, uint32 entity){
 	printf("%d watch %d move!", watcher, entity);
+}
+static void
+_free_entity(EntityPtr e){
+	EntityPtr t = NULL;
+	while (e) {
+		t = e;
+		e = e->next;
+		free(t);
+	}
+}
+static GIDX
+_grid_idx0(AOIPtr thiz, GIDX gx, GIDX gy) {
+	return gx * thiz->gylen + gy;
+}
+static inline GIDX
+_grid_idx(AOIPtr thiz, uint16 x, uint16 y) {
+	return _grid_idx0(thiz, x >> thiz->gxbit, y >> thiz->gybit);
+}
+static inline bool
+_in_sight(EntityPtr w, EntityPtr e){
+	return w->r >= abs(w->x - e->x) && w->r >= abs(w->y - e->y);
+}
+static GIDX
+_iter_watcher(EntityPtr w, EntityPtr e, aoi_cb cb) {
+	while (w) {
+		if (_in_sight(w, e))//todo 自己格子内不需要比较
+			cb(w->id, e->id);
+		w = w->next;
+	}
+}
+static void
+_iter_grid_row(AOIPtr thiz, GIDX gidx, EntityPtr e, aoi_cb cb) {
+	_iter_watcher(thiz->vGrid[gidx].watchers, e, cb);
+	if (gidx % thiz->gylen != 0)
+		_iter_watcher(thiz->vGrid[gidx - 1].watchers, e, cb);
+	if ((gidx + 1) % thiz->gylen != 0)
+		_iter_watcher(thiz->vGrid[gidx + 1].watchers, e, cb);
+}
+static void
+_iter_grid9(AOIPtr thiz, EntityPtr e, aoi_cb cb){
+	_iter_grid_row(AOIPtr thiz, e->gidx, e, cb);
+	if (e->gidx >= thiz->gylen)
+		_iter_grid_row(AOIPtr thiz, e->gidx - thiz->gylen, e, cb);
+	if (e->gidx < (thiz->gylen - thiz->gxlen))
+		_iter_grid_row(AOIPtr thiz, e->gidx + thiz->gylen, e, cb);
+}
+static void
+_add_entity(EntityPtr *head, EntityPtr e) {
+	e->next = *head;
+	*head = e;
+}
+static void
+_del_entity(EntityPtr *head, EntityPtr prev, EntityPtr e) {
+	if (prev)
+		prev->next = e->next;
+	else
+		*head = e->next;
 }
 
 AOIPtr
@@ -46,71 +105,22 @@ aoi_init(uint16 mx, uint16 my, uint8 gx_bit, uint8 gy_bit){
 	AOIPtr thiz = malloc(sizeof(*thiz));
 	thiz->mx = mx;
 	thiz->my = my;
-	thiz->gx_bit = gx_bit;
-	thiz->gy_bit = gy_bit;
-	thiz->xlen = ((mx - 1) >> gx_bit) + 1;
-	thiz->ylen = ((my - 1) >> gy_bit) + 1;
-	thiz->vGrid = calloc(sizeof(*thiz->vGrid), thiz->xlen * thiz->ylen);
+	thiz->gxbit = gx_bit;
+	thiz->gybit = gy_bit;
+	thiz->gxlen = ((mx - 1) >> gx_bit) + 1;
+	thiz->gylen = ((my - 1) >> gy_bit) + 1;
+	thiz->vGrid = calloc(sizeof(*thiz->vGrid), thiz->gxlen * thiz->gylen);
 }
-
 void
 aoi_free(AOIPtr thiz){
 	assert(thiz);
-	EntityPtr e = NULL, t = NULL;
-	for (uint8 i = 0; i < thiz->xlen * thiz->ylen; ++i){
-		e = thiz->vGrid[i]->watchers;
-		while (e){
-			t = e;
-			e = e->next;
-			free(t);
-		}
-		e = thiz->vGrid[i]->markers;
-		while (e){
-			t = e;
-			e = e->next;
-			free(t);
-		}
+	for (uint8 i = 0; i < thiz->gxlen * thiz->gylen; ++i){
+		_free_entity(thiz->vGrid[i].watchers);
+		_free_entity(thiz->vGrid[i].markers);
 	}
 	free(thiz->vGrid);
 	free(thiz);
 }
-
-static GIDX
-_grid_idx(AOIPtr thiz, uint16 x, uint16 y){
-	return (x >> thiz->gx_bit) * thiz->ylen + (y >> thiz->gy_bit);
-}
-
-static GIDX
-_iter_watcher(EntityPtr pEntity, uint32 eid, aoi_cb cb){
-	while (pEntity){
-		if (pEntity->id != eid)//??
-			cb(pEntity->id, eid);
-		pEntity = pEntity->next;
-	}
-}
-
-static void
-_iter_grid_row(AOIPtr thiz, GIDX egidx, uint32 eid, aoi_cb cb){
-	_iter_watcher(thiz->vGrid[egidx]->watchers, eid, cb);
-	GIDX n = (thiz->my >> thiz->gy_bit);
-	if (egidx > 0 && ((egidx - 1) % n) != (n - 1))
-		_iter_watcher(thiz->vGrid[egidx - 1]->watchers, eid, cb);
-	if ((egidx + 1) % n != 0)
-		_iter_watcher(thiz->vGrid[egidx + 1]->watchers, eid, cb);
-}
-
-static void
-_iter_grid9(AOIPtr thiz, GIDX egidx, uint32 eid, aoi_cb cb){
-	GIDX n = (thiz->my >> thiz->gy_bit);
-	_iter_grid_row(AOIPtr thiz, egidx, eid, cb);
-	if (egidx >= n)
-		_iter_grid_row(AOIPtr thiz, egidx - n, eid, cb);
-	if (egidx < (thiz->glen - n))
-		_iter_grid_row(AOIPtr thiz, egidx + n, eid, cb);
-}
-
-
-
 GIDX
 aoi_add(AOIPtr thiz, uint32 entity, uint16 x, uint16 y, uint16 r, bool w){
 	assert(x < thiz->mx && y < thiz->my);
@@ -120,42 +130,36 @@ aoi_add(AOIPtr thiz, uint32 entity, uint16 x, uint16 y, uint16 r, bool w){
 	e->y = y;
 	e->r = r;
 	e->gidx = _grid_idx(thiz, x, y);
-
-	EntityPtr h = NULL;
-	if (w)
-		h = thiz->vGrid[e->gidx]->watchers;
-	else
-		h = thiz->vGrid[e->gidx]->markers;
-
-	e->next = h;
-	h = e;
-	_iter_grid9(thiz, e->gidx, e->id, cb_appear);
+	_iter_grid9(thiz, e, cb_appear);
+	_add_entity(w ? &thiz->vGrid[e->gidx].watchers : &thiz->vGrid[e->gidx].markers, e);
 	return e->gidx;
 }
 
 void
-aoi_move(AOIPtr thiz, GIDX gx, GIDX gy, bool w, uint32 entity, uint16 x, uint16 y){
+aoi_move(AOIPtr thiz, GIDX gidx, bool w, uint32 entity, uint16 x, uint16 y){
 	assert(x < thiz->mx && y < thiz->my);
-	assert(gx < thiz->xlen && gy < thiz->ylen);
-	EntityPtr e = NULL;
-	if (w)
-		e = thiz->vGrid[gx * thiz->ylen + ]->watchers;
-	else
-		e = thiz->vGrid[gidx]->markers;
-	while (e){//tofix
-		if (e->id == entity)
-			break;
+	assert(gidx < (thiz->gxlen * thiz->gylen));
+	EntityPtr e = w ? thiz->vGrid[gidx]->watchers : thiz->vGrid[gidx]->markers;
+	EntityPtr prev = NULL;
+	while (e && e->id != entity) {//iterator one grid //todo
+		prev = e;
 		e = e->next;
 	}
 	assert(e->id == entity && e->gidx == gidx);
 	assert(e->x != x || e->y != y);
-	e->x = x;
-	e->y = y;
-	e->gidx = _grid_idx(thiz, x, y);
-	if (e->gidx != gidx){
-		_iter_grid9(thiz, gidx, e->id, cb_disappear);
-		_iter_grid9(thiz, e->gidx, e->id, cb_appear);
+	gidx = _grid_idx(thiz, x, y);
+	if (e->gidx != gidx) {
+		_del_entity(w ? &thiz->vGrid[gidx].watchers : &thiz->vGrid[gidx].markers, prev, e);
+		_iter_grid9(thiz, e, cb_disappear);
+		e->x = x;
+		e->y = y;
+		e->gidx = gidx;
+		_iter_grid9(thiz, e, cb_appear);
+		_add_entity(w ? &thiz->vGrid[e->gidx].watchers : &thiz->vGrid[e->gidx].markers, e);
 	}
-	else
-		_iter_grid9(thiz, e->gidx, e->id, cb_move);
+	else {
+		e->x = x;
+		e->y = y;
+		_iter_grid9(thiz, e->gidx, e->id, cb_move);//todo 移动事件中增加出视野 进视野判断
+	}
 }
