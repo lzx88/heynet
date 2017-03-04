3adc0bf54782c40895914a50946f78e210596fc2
lzx88 <lvzx538001@163.com>
2017-03-05 00:16:34 +0800

    fdf

--- server/lualib-c/zproto/zproto.h
+++ server/lualib-c/zproto/zproto.h
@@ -14,22 +14,15 @@ struct field;
 struct type;
 struct protocol;
 struct zproto;
-struct zproto_arg {
-	const struct field *pf;
-	void *ud;
-	void *value;
-	int length;
-	int index;
-	char shift;
-};
+
 typedef int64 integer;
 
 #define _shift16(p) (int16)p[1] | (int16)p[0] << 8
 #define _shift32(p) (int16)p[3] | (int16)p[2] << 8 | (int32)p[1] << 16 | (int32)p[0] << 24
 #define _shift64(p) (int16)p[7] | (int16)p[6] << 8 | (int32)p[5] << 16 | (int32)p[4] << 24 | (int64)p[3] << 32 | (int64)p[2] << 40 | (int64)p[1] << 48 | (int64)p[0] << 56
-static int16 shift16(const char* p, bool shift){ return shift ? _shift16(p) : *(int16*)p; }
-static int32 shift32(const char* p, bool shift){ return shift ? _shift32(p) : *(int32*)p; }
-static int64 shift64(const char* p, bool shift){ return shift ? _shift64(p) : *(int64*)p; }
+int16 shift16(const char* p, bool shift){ return shift ? _shift16(p) : *(int16*)p; }
+int32 shift32(const char* p, bool shift){ return shift ? _shift32(p) : *(int32*)p; }
+int64 shift64(const char* p, bool shift){ return shift ? _shift64(p) : *(int64*)p; }
 
 void zproto_init(struct zproto *thiz);
 void *zproto_alloc(struct zproto *thiz, size_t sz);
@@ -43,10 +36,17 @@ const struct protocol *zproto_findtag(const struct zproto *thiz, int tag);
 #define ZPROTO_CB_MEM		-1
 #define ZPROTO_CB_ERR		-2
 #define ZPROTO_CB_NIL		-3
-
+struct zproto_arg {
+	const struct field *pf;
+	void *ud;
+	void *value;
+	int length;
+	int index;
+	char shift;
+};
+int zproto_pack(const char* data, int size, char *buffer, int len);
+int zproto_unpack(const char *data, int size, char *buffer, int len);
 typedef int(*zproto_cb)(struct zproto_arg *args);
 int zproto_encode(const struct type *ty, char *buffer, int size, zproto_cb cb, void *ud);
 int zproto_decode(const struct type *ty, const char *data, int size, bool shift, zproto_cb cb, void *ud);
-int zproto_pack(const char* data, int size, char *buffer, int len);
-int zproto_unpack(const char *data, int size, char *buffer, int len);
 #endif
