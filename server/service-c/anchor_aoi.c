39c14437d4fc4d150c5c73271a2af355593bf18b
joelv(吕志雄) <joelv@KUGOU.GZ>
2017-06-15 18:32:15 +0800

    fdf

--- server/service-c/anchor_aoi.c
+++ server/service-c/anchor_aoi.c
@@ -9,16 +9,16 @@ typedef struct Entity{
 	uint16 radius;
 }*EntityPtr;
 
-#define X 0
-#define Y 1
-#define XY 2
-#define NEXT 0
-#define PREV 1
-#define PN 2
-#define MAX_REDIUS 100//最大视野半径
+#define _X 0
+#define _Y 1
+#define _XY 2
+#define _NEXT 0
+#define _PREV 1
+#define _PN 2
+#define _MAX_REDIUS 100//最大视野半径
 
 typedef struct OLNode{
-	struct OLNode* list[XY][PN];
+	struct OLNode* list[_XY][_PN];
 	uint16 x;
 	uint16 y;
 	EntityPtr entitys;
@@ -30,7 +30,7 @@ typedef struct OlHeader{
 }*OlHeaderPtr;
 
 typedef struct AOI{
-	OlHeader header[XY];
+	OlHeader header[_XY];
 }*AOIPtr;
 
 static void
@@ -49,18 +49,18 @@ _free_entity(EntityPtr e){
 }
 static void
 _free_olnode(OLNodePtr node, uint8 dir){
-	assert(dir < XY);
+	assert(dir < _XY);
 	OLNodePtr t = NULL;
 	while (node) {
 		t = node;
-		node = t->list[dir][NEXT];
+		node = t->list[dir][_NEXT];
 		_free_entity(t->entitys);
 		free(t);
 	}
 }
 static OLNodePtr*
 _get_olhead(AOIPtr thiz, uint8 dir, uint16 i){
-	assert(dir < XY);
+	assert(dir < _XY);
 	assert(i < thiz->header[dir].size);
 	return &thiz->header[dir].ptr[i];
 }
@@ -72,70 +72,89 @@ static bool
 _is_empty_olhead(AOIPtr thiz, uint8 dir, uint16 i){
 	uint8 direct;
 	switch (dir) {
-	case X: direct = Y; break;
-	case Y: direct = X; break;
+	case _X: direct = _Y; break;
+	case _Y: direct = _X; break;
 	default: assert(false);
 	}
 	OLNodePtr node = *_get_olhead(thiz, dir, i);
 	while (node) {
 		if (!_is_empty_olnode(node))
 			return false;
-		node = node->list[direct][NEXT];
+		node = node->list[direct][_NEXT];
 	}
 	return true;
 }
+static void
+_insert_olnode(OLNodePtr* xhead, OLNodePtr* yhead, OLNodePtr node){
+
+}
 
 static OLNodePtr
-_add_anchor(AOIPtr thiz, uint16 x, uint16 y){
-	OLNodePtr* xpnode = _get_olhead(thiz, X, x);
+_get_anchor(AOIPtr thiz, uint16 x, uint16 y){
+	OLNodePtr* xpnode = _get_olhead(thiz, _X, x);
 
 	OLNodePtr target = *xpnode;
 	while (target && target->y < y)
-		target = target->list[Y].[NEXT];
+		target = target->list[_Y].[_NEXT];
 	if (target && target->y == y)
 		return target;
 
-	OLNodePtr node = *xpnode;
-	for (int iy = 0; iy < thiz->header[Y].size; ++iy){
-		if (node && node->y == iy){
-			node = node->list[Y].[NEXT];
+	OLNodePtr nodey = *xpnode;
+	for (int iy = 0; iy < thiz->header[_Y].size; ++iy){
+		if (nodey && nodey->y == iy){
+			nodey = nodey->list[_Y].[_NEXT];
 			continue;
 		}
-		if (!_is_empty_olhead(thiz, Y, iy)){
-			target = malloc(sizeof(*target));
-			target->entitys = NULL;
-			target->x = x;
-			target->y = iy;
-			target->list[]
-			if (node == NULL){
-
-			}
+		if (!_is_empty_olhead(thiz, _Y, iy)){
+			OLNodePtr nnode = malloc(sizeof(*nnode));
+			nnode->entitys = NULL;
+			nnode->x = x;
+			nnode->y = iy;
+			_insert_olnode(xpnode, _get_olhead(thiz, _Y, iy), nnode);
+			if (iy = y)
+				target = nnode;
 		}
+	}
 
-
+	OLNodePtr* ypnode = _get_olhead(thiz, _Y, y);
+	OLNodePtr nodex = *ypnode;
+	for (int ix = 0; ix < thiz->header[_X].size; ++ix){
+		if (nodex && nodex->x == ix){
+			nodex = nodex->list[_X].[_NEXT];
+			continue;
+		}
+		if (!_is_empty_olhead(thiz, _X, ix)){
+			OLNodePtr nnode = malloc(sizeof(*nnode));
+			nnode->entitys = NULL;
+			nnode->x = ix;
+			nnode->y = y;
+			_insert_olnode(xpnode, ypnode, nnode);
+			if (ix = x)
+				target = nnode;
+		}
 	}
 
 
 
-	return newnode;
+	return target;
 }
 
 
 void
 aoi_free(AOIPtr thiz){
 	assert(thiz);
-	for (uint16 i = 0; i < thiz->header[X].size; ++i)
-		_free_olnode(thiz->header[X].ptr[i], Y);
-	free(thiz->header[X].ptr);
-	free(thiz->header[Y].ptr);
+	for (uint16 i = 0; i < thiz->header[_X].size; ++i)
+		_free_olnode(thiz->header[_X].ptr[i], _Y);
+	free(thiz->header[_X].ptr);
+	free(thiz->header[_Y].ptr);
 	free(thiz);
 	thiz = NULL;
 }
 AOIPtr
 aoi_init(uint16 mx, uint16 my){
 	AOIPtr thiz = malloc(sizeof(*thiz));
-	_init_olheader(&thiz->header[X], mx);
-	_init_olheader(&thiz->header[Y], my);
+	_init_olheader(&thiz->header[_X], mx);
+	_init_olheader(&thiz->header[_Y], my);
 }
 
 static void
@@ -202,8 +221,8 @@ _iter_quad(OLNodePtr origin, EntityPtr entity, aoi_cb cb, uint8 xy, uint8 pn, ui
 		header = header->list[xy][pn];
 		if (header == NULL)
 			break;
-		uint16 distance = xy == X ? abs(header->x - origin->x) : abs(header->y - origin->y);
-		if (distance > MAX_REDIUS)
+		uint16 distance = xy == _X ? abs(header->x - origin->x) : abs(header->y - origin->y);
+		if (distance > _MAX_REDIUS)
 			break;
 		_iter_entity(header->entitys, entity, distance, cb);
 		if (iter_cb)
@@ -213,16 +232,16 @@ _iter_quad(OLNodePtr origin, EntityPtr entity, aoi_cb cb, uint8 xy, uint8 pn, ui
 void
 _iter_watcher(OLNodePtr origin, EntityPtr entity, aoi_cb cb){
 	_iter_entity(origin, entity, 0, cb);
-	_iter_quad(origin, entity, cb, X, PREV, Y, PREV, _iter_quad);
-	_iter_quad(origin, entity, cb, Y, PREV, X, NEXT, _iter_quad);
-	_iter_quad(origin, entity, cb, X, NEXT, Y, NEXT, _iter_quad);
-	_iter_quad(origin, entity, cb, Y, NEXT, X, PREV, _iter_quad);
+	_iter_quad(origin, entity, cb, _X, _PREV, _Y, _PREV, _iter_quad);
+	_iter_quad(origin, entity, cb, _Y, _PREV, _X, _NEXT, _iter_quad);
+	_iter_quad(origin, entity, cb, _X, _NEXT, _Y, _NEXT, _iter_quad);
+	_iter_quad(origin, entity, cb, _Y, _NEXT, _X, _PREV, _iter_quad);
 }
 
 void
 aoi_add(AOIPtr thiz, uint32 entity, uint16 x, uint16 y, uint16 radius){
-	assert(x < thiz->header[X].size && y < thiz->header[Y].size && radius <= MAX_REDIUS);
-	OLNodePtr header = _add_anchor(thiz, x, y);
+	assert(x < thiz->header[_X].size && y < thiz->header[_Y].size && radius <= _MAX_REDIUS);
+	OLNodePtr header = _get_anchor(thiz, x, y);
 	assert(header);
 	EntityPtr e = malloc(sizeof(*e));
 	e->id = entity;
@@ -233,7 +252,7 @@ aoi_add(AOIPtr thiz, uint32 entity, uint16 x, uint16 y, uint16 radius){
 
 void
 aoi_del(AOIPtr thiz, uint32 entity, uint16 x, uint16 y){
-	assert(x < thiz->header[X].size && y < thiz->header[Y].size);
+	assert(x < thiz->header[_X].size && y < thiz->header[_Y].size);
 	_del_entity(thiz->xHeader[x], e, x, y);
 }
 
